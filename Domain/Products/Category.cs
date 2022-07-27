ce66bad8208d559d58f563faaf05396449d035d7
Renan Kimura <kimura@live.it>
2022-07-27 09:35:07 -0300

    Seção 13 completa

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -4,17 +4,10 @@ namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; }
-    public bool Active { get; set; }
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
     public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>()
-            .IsNotNullOrEmpty(name, "Name")
-            .IsGreaterOrEqualsThan(name, 3, "Name")
-            .IsNotNullOrEmpty(createdBy, "CreatedBy")
-            .IsNotNullOrEmpty(editedBy, "EditedBy");
-        AddNotifications(contract);
-
         Name = name;
         Active = true;
         CreatedBy = createdBy;
@@ -22,5 +15,25 @@ public class Category : Entity
         CreatedOn = DateTime.Now;
         EditedOn = DateTime.Now;
 
+        Validate();
+
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(Name, "Name")
+            .IsGreaterOrEqualsThan(Name, 3, "Name")
+            .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
+            .IsNotNullOrEmpty(EditedBy, "EditedBy");
+        AddNotifications(contract);
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validate();
     }
 }
