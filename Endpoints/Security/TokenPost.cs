72ede4aa625dba24abacb188c2271ae31c93870b
Renan Kimura <kimura@live.it>
2022-08-04 13:34:09 -0300

    Serilog funcionando

--- Endpoints/Security/TokenPost.cs
+++ Endpoints/Security/TokenPost.cs
@@ -13,9 +13,13 @@ public class TokenPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
     [AllowAnonymous]
-    public static IResult Action(LoginRequest loginRequest, IConfiguration configuration, UserManager<IdentityUser> userManager)
+    public static IResult Action(LoginRequest loginRequest, IConfiguration configuration, UserManager<IdentityUser> userManager, ILogger<TokenPost> log)
     {
-       var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
+        log.LogInformation("Getting token");
+        log.LogWarning("Warning");
+        log.LogError("Error");
+
+        var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
         if (user == null) 
             Results.BadRequest();
         if (!userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
